5e0172cefe4bfc9b9a8d4501d7c8eda03d2b96b9
Marcos Imfeld <31708380+markimfeld@users.noreply.github.com>
2025-08-14 22:15:26 -0300

    Implement tests delete accounts (#39)
    Co-authored-by: Marcos Imfeld <macaroni.app@gmail.com>

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -2,7 +2,7 @@ import { IAccount } from "@/models/account.model";
 import { CreateAccountDto } from "../dtos/createAccount.dto";
 import { IAccountRepository } from "../interfaces/repositories/account.repository.interface";
 import { UpdateAccountDto } from "../dtos/updateAccount.dto";
-import { NotFoundError } from "../errors";
+import { BadRequestError, NotFoundError } from "../errors";
 import { MESSAGES } from "../constants/messages";
 import { PaginatedResult } from "../dtos/paginatedResult.dto";
 
@@ -53,6 +53,12 @@ export class AccountService {
 
     if (!account || account.userId.toString() !== userId || account.isDeleted) {
       throw new NotFoundError(MESSAGES.ERROR.ACCOUNT.NOT_FOUNTD);
+    }
+
+    if (account.balance > 0) {
+      throw new BadRequestError(
+        MESSAGES.ERROR.ACCOUNT.CANNOT_DELETE_ACCOUNT_WITH_BALANCE_GREATER_THAN_ZERO
+      );
     }
 
     await this.accountRepo.softDeleteAccountById(accountId, userId);
